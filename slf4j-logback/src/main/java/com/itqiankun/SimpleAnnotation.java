90b31a0166f7f10904e0893d0610b06e70160ef6
ma_qiankun <ma_qiankun@hzlh.com>
2023-03-21 10:54:01 +0800

    modify

---
+++ slf4j-logback/src/main/java/com/itqiankun/SimpleAnnotation.java
@@ -0,0 +1,19 @@
+package com.itqiankun;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: ma_qiankun
+ * @date: 2023/3/21
+ **/
+@Slf4j
+public class SimpleAnnotation {
+
+    @Test
+    public void test (){
+        log.info("学IT，找IT乾坤，官网地址： {}","https://www.itqiankun.com");
+    }
+}
